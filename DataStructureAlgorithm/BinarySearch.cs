112d9cdaa7a78db73160f233fa693ed5f885e4a1
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-22 22:56:12 +0530

    [Lalit] Add: Binary Search the word from the WordList

---
+++ DataStructureAlgorithm/BinarySearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BinarySearch
+    {
+        string[] binaryArr;
+        public void ReadTextFiles(string filePath)
+        {
+            string words = File.ReadAllText(filePath);
+            binaryArr = words.Split(",");
+        }
+        public void BinarySearchOperation()
+        {
+            string input = Console.ReadLine().ToLower();
+            foreach (var words in binaryArr)
+            {
+                if (words.Equals(input))
+                {
+                    Console.WriteLine("{0} - Yupiee, Found the word in the list using binary search");
+                    return;
+                }
+            }
+            Console.WriteLine("Searched element is not available in the lsit of words");
+
+        }
+        public void Display()
+        {
+            foreach (var data in binaryArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataStructureAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 2, 5, 7, 6, 1, 4 };
+            Console.WriteLine("Welcome to Data Structure Algorithm");
+            BinarySearch binary = new BinarySearch();
+            binary.ReadTextFiles(@"D:\Projects-Bridgelabz\DataStructureAlgorithm\DataStructureAlgorithm\Files\BinarySearch.txt");
+            binary.BinarySearchOperation();
+        }
+    }
+}
