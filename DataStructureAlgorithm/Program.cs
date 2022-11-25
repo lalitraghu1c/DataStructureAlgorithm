c27a949d6d9cd2ed30596b668be60f6a0cf4cf67
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-11-26 00:59:09 +0530

    [Lalit] Add: Replacing String via User

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -6,24 +6,25 @@ namespace DataStructureAlgorithm
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Algorithm");
-            BinarySearch binary = new BinarySearch();
             bool condition = true;
             while (condition)
             {
-                Console.WriteLine("1. Binary Search\n2. Insertion Sorting\n3. Bubble Sort\n4. Anagram\n5. Exit");
+                Console.WriteLine("1. Binary Search\n2. Insertion Sorting\n3. Bubble Sort\n4. Anagram\n5. Replace the String using Regex\n6. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
                         Console.WriteLine("Enter the word you want to Search");
+                        BinarySearch binary = new BinarySearch();
                         binary.ReadTextFiles(@"D:\Projects-Bridgelabz\DataStructureAlgorithm\DataStructureAlgorithm\Files\BinarySearch.txt");
                         binary.BinarySearchOperation();
                         Console.WriteLine("\n");
                         break;
                     case 2:
+                        BinarySearch binary1 = new BinarySearch();
                         Console.WriteLine("\n");
                         int[] arr = { 2, 5, 7, 6, 1, 4 };
-                        binary.Sort(arr);
+                        binary1.Sort(arr);
                         Console.WriteLine("\n");
                         break;
                     case 3:
@@ -36,6 +37,10 @@ namespace DataStructureAlgorithm
                     case 4:
                         Anagram anagram = new Anagram();
                         anagram.CheckAnagram("heart" , "earth");
+                        break;
+                    case 5:
+                        Replace replace = new Replace();
+                        replace.ReplaceString();
                         break;
                     default:
                         Console.WriteLine("Try Again");
---
+++ DataStructureAlgorithm/Replace.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+
+{
+    public class Replace
+    {
+        public void ReplaceString()
+        {
+            string input = "Hello <<name>>, \nWe have your fullname as <<full name>> in our system. \nYour contact number is 91-<<xxxxxxxxxx>>.\nPlease,let us know in case of any clarification Thank you BridgeLabz 01/01/2016.";
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+            input = input.Replace("<<name>>",name);
+            Console.WriteLine("Enter your full name");
+            string fullName = Console.ReadLine();
+            input = input.Replace("<full name>", fullName);
+            Console.WriteLine("Enter your Mobile No.");
+            string mobile = Console.ReadLine();
+            input = input.Replace("<xxxxxxxxxx>", mobile);
+
+            Console.WriteLine(input);
+        }
+    }
+}
